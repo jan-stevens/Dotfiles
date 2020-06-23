5ca9e3ee3a697453cacff304a491afcc38dec8dd
biogen98 <jan.adriaan.stevens98@gmail.com>
2020-06-23 23:53:06 +0200

    update

--- polybar/.config/polybar/scripts/network.sh
+++ polybar/.config/polybar/scripts/network.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-WIFI_STATUS=$(cat /sys/class/net/wlp2s0/operstate);
-CONNECTION=$(nmcli -t -f name connection show --active);
+
+
+#WIFI_STATUS=$(cat /sys/class/net/wlp2s0/operstate);
+WIFI_STATUS=$(cat /sys/class/net/wlan0/operstate);
+#CONNECTION=$(nmcli -t -f name connection show --active);
+CONNECTION=$(connmanctl services | awk  '{print $2}');
+
 if [ "$WIFI_STATUS" = "up" ];
 then
   ICON="";
